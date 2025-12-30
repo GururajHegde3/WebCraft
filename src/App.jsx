865c5830b9bacfbd1a9a2a0c05f911bb7b6d02c0
Gururaj Hegde <rguru7825@gmail.com>
2025-12-30 11:42:25 +0530

    Add Analytics import to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Portfolio from './components/Portfolio.jsx'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   const [count, setCount] = useState(0)
